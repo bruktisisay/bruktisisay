8ebcc4775c761cfb2d16bf013b5e59c1edf072f4
bruktisisay <bruktisisay052@gmail.com>
2024-05-09 07:13:17 -0700

    first commit

---
+++ portfolio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'yz6hq6vz',
+    dataset: 'production'
+  }
+})
---
+++ portfolio/schemaTypes/index.ts
@@ -0,0 +1,10 @@
+import skill from "./skill";
+import testimonials from "./testimonials";
+import contact from "./contact.js"
+import workExperienes from "./workExperienes";
+import abouts from "./abouts";
+import works from "./works";
+import experiences from "./experiences";
+import brands from "./brands";
+
+export const schemaTypes = [testimonials,skill,contact,workExperienes,abouts,works,experiences,brands]
